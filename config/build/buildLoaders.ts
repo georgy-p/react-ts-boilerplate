8d1353abd6ce38552250504dfde8c16f55e8f8c1
Georgy <pigargeorge@gmail.com>
2023-09-30 12:29:11 +0300

    delete class name hash

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -19,7 +19,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
                         auto: (resPath: string) =>
                             Boolean(resPath.includes('.module.')),
                         localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:5]'
+                            ? '[path][name]__[local]'
                             : '[hash:base64:8]',
                     },
                 },
